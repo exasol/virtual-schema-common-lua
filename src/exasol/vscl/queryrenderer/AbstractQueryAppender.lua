fff78f4cac9f881af86c1c376712e465ccc8f6c1
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-09-06 12:58:25 +0200

    Fix argument type annotations

--- src/exasol/vscl/queryrenderer/AbstractQueryAppender.lua
+++ src/exasol/vscl/queryrenderer/AbstractQueryAppender.lua
@@ -32,7 +32,7 @@ function AbstractQueryAppender:_comma(index)
     end
 end
 
----@param data_type ExasolTypeDefinition
+---@param data_type DecimalTypeDefinition
 function AbstractQueryAppender:_append_decimal_type_details(data_type)
     self:_append("(")
     self:_append(data_type.precision)
@@ -41,7 +41,7 @@ function AbstractQueryAppender:_append_decimal_type_details(data_type)
     self:_append(")")
 end
 
----@param data_type ExasolTypeDefinition
+---@param data_type CharacterTypeDefinition
 function AbstractQueryAppender:_append_character_type(data_type)
     self:_append("(")
     self:_append(data_type.size)
@@ -53,14 +53,14 @@ function AbstractQueryAppender:_append_character_type(data_type)
     end
 end
 
----@param data_type ExasolTypeDefinition
+---@param data_type TimestampTypeDefinition
 function AbstractQueryAppender:_append_timestamp(data_type)
     if data_type.withLocalTimeZone then
         self:_append(" WITH LOCAL TIME ZONE")
     end
 end
 
----@param data_type ExasolTypeDefinition
+---@param data_type GeometryTypeDefinition
 function AbstractQueryAppender:_append_geometry(data_type)
     local srid = data_type.srid
     if srid then
@@ -99,7 +99,7 @@ function AbstractQueryAppender:_append_interval(data_type)
     end
 end
 
----@param data_type ExasolTypeDefinition
+---@param data_type HashtypeTypeDefinition
 function AbstractQueryAppender:_append_hashtype(data_type)
     local byte_size = data_type.bytesize
     if byte_size then
